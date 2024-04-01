a52f879eea51851dd8ab12bed2ca9cee56bb9b99
nvhieu94 <caubepoor94@gmail.com>
2024-04-02 00:20:50 +0700

    check deploy

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ function App() {
   return (
     <div className="App">
       <QuizMakerProvider>
-        <RouterProvider router={router} to="/" />
+        <RouterProvider router={router} />
         {/* <QuizMaker /> */}
       </QuizMakerProvider>
    
